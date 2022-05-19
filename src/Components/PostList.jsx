4b25b8d8f0434aa0b42e31b72ff8d2bb71378a96
7aso0ni <53603723+7aso0ni@users.noreply.github.com>
2022-05-19 14:26:53 +0300

    new!!

---
+++ src/Components/PostList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions/action';
+
+class PostList extends React.Component {
+    componentDidMount() {
+        this.props.fetchPosts()
+    }
+
+    render() {
+        return (
+            <div>
+                Post List
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return null;
+}
+
+export default connect(null, { fetchPosts })(PostList)
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import App from './Components/App';
+import reducers from './reducers/reducers';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {createStore(reducers)}> 
+        <App />
+    </Provider>
   </React.StrictMode>
 );
 
